86701a1da05b828f700e63f3a9143d97d892aa3e
vlad <sumskyi@gmail.com>
2013-02-10 13:30:23 +0200

    p3 done

--- features/filter_movie_list.feature
+++ features/filter_movie_list.feature
@@ -1,5 +1,5 @@
 Feature: display list of movies filtered by MPAA rating
- 
+
   As a concerned parent
   So that I can quickly browse movies appropriate for my family
   I want to see movies matching only certain MPAA ratings
@@ -20,16 +20,32 @@ Background: movies have been added to database
   | Chicken Run             | G      | 21-Jun-2000  |
 
   And  I am on the RottenPotatoes home page
-  
+
 Scenario: restrict to movies with 'PG' or 'R' ratings
-  # enter step(s) to check the 'PG' and 'R' checkboxes
-  # enter step(s) to uncheck all other checkboxes
-  # enter step to "submit" the search form on the homepage
-  # enter step(s) to ensure that PG and R movies are visible
-  # enter step(s) to ensure that other movies are not visible
+  Given I check the following ratings: PG, R
+  And I uncheck the following ratings: G, PG-13
+  And I press "ratings_submit"
+  Then I should see the following movies:
+    | title                   | rating  |
+    | The Incredibles         | PG      |
+    | Raiders of the Lost Ark | PG      |
+    | The Terminator          | R       |
+    | When Harry Met Sally    | R       |
+    | Amelie                  | R       |
+  And I should not see the following movies:
+    | title                   | rating  |
+    | Aladdin                 | PG      |
+    | 2001: A Space Odyssey   | PG      |
+    | Chicken Run             | PG      |
+    | The Help                | PG-13   |
+    | Chocolat                | PG-13   |
 
 Scenario: no ratings selected
-  # see assignment
+  Given I uncheck the following ratings: G, PG-13, R, PG
+  And I press "Refresh"
+  Then I should see no movies
 
 Scenario: all ratings selected
-  # see assignment
+  Given I check the following ratings: G, PG-13, R, PG
+  And I press "Refresh"
+  Then I should see all of the movies
--- features/step_definitions/movie_steps.rb
+++ features/step_definitions/movie_steps.rb
@@ -2,19 +2,29 @@
 
 Given /the following movies exist/ do |movies_table|
   movies_table.hashes.each do |movie|
-    # each returned element will be a hash whose key is the table header.
-    # you should arrange to add that movie to the database here.
+    Movie.create!(movie)
   end
-  flunk "Unimplemented"
 end
 
 # Make sure that one string (regexp) occurs before or after another one
 #   on the same page
 
-Then /I should see "(.*)" before "(.*)"/ do |e1, e2|
-  #  ensure that that e1 occurs before e2.
-  #  page.body is the entire content of the page as a string.
-  flunk "Unimplemented"
+Then /I should see "(.*)" before "(.*)"/ do |movie1, movie2|
+  pos1 = page.body.index(movie1)
+  pos2 = page.body.index(movie2)
+  assert pos1 < pos2
+end
+
+Given /^I should see the following movies:$/ do |table|
+  table.hashes.each do |movie|
+    step %{I should see "#{movie['title']}"}
+  end
+end
+
+Given /^I should not see the following movies:$/ do |table|
+  table.hashes.each do |movie|
+    step %{I should not see "#{movie['title']}"}
+  end
 end
 
 # Make it easier to express checking or unchecking several boxes at once
@@ -22,7 +32,20 @@ end
 #  "When I check the following ratings: G"
 
 When /I (un)?check the following ratings: (.*)/ do |uncheck, rating_list|
-  # HINT: use String#split to split up the rating_list, then
-  #   iterate over the ratings and reuse the "When I check..." or
-  #   "When I uncheck..." steps in lines 89-95 of web_steps.rb
+  rating_list.split(/[, ]+/).each do |el|
+    if uncheck
+      step %{I uncheck "ratings_#{el}"}
+    else
+      step %{I check "ratings_#{el}"}
+    end
+  end
 end
+
+Then /^I should see no movies$/ do
+  assert_equal 0, page.all('table tbody tr').size
+end
+
+Then /^I should see all of the movies$/ do
+  assert_equal Movie.count, page.all('table tbody tr').size
+end
+
